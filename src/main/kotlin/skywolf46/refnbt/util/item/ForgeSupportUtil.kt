8b4a3e54d5d9939a68475dbae48954669f45b486
Wat da fak <juni2484ok@naver.com>
2021-04-14 20:48:29 +0900

    ReflectedNBTWrapper 1.7.2.6 - Now supporting CatServer item conversion

---
+++ src/main/kotlin/skywolf46/refnbt/util/item/ForgeSupportUtil.kt
@@ -0,0 +1,32 @@
+package skywolf46.refnbt.util.item
+
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import skywolf46.refnbt.util.BukkitVersionUtil
+
+private val NMS_ITEM_CONSTRUCTOR = BukkitVersionUtil.getNMSClass("ItemStack")
+    .getConstructor(BukkitVersionUtil.getNMSClass("Item"),
+        Int::class.javaPrimitiveType,
+        Int::class.javaPrimitiveType,
+        Boolean::class.javaPrimitiveType)
+private val NMS_ITEM_CLONE = BukkitVersionUtil.getNMSClass("ItemStack").getMethod("cloneItemStack")
+private val NMS_ITEM_LOAD =
+    BukkitVersionUtil.getNMSClass("ItemStack").getMethod("load", BukkitVersionUtil.getNMSClass("NBTTagCompound"))
+
+private val COPY_METHOD = BukkitVersionUtil.getOBCClass("inventory.CraftItemStack")
+    .getMethod("asCraftMirror", BukkitVersionUtil.getNMSClass("ItemStack"))
+
+private val MAGIC_NUMBER_GETTER =
+    BukkitVersionUtil.getOBCClass("util.CraftMagicNumbers").getMethod("getItem", Int::class.javaPrimitiveType)
+
+// Only works on CatServer
+fun ItemStack.asBukkitItem(): org.bukkit.inventory.ItemStack {
+//    return COPY_CONSTRUCTOR.newInstance(this) as org.bukkit.inventory.ItemStack
+    val item =
+        NMS_ITEM_CONSTRUCTOR.newInstance(MAGIC_NUMBER_GETTER.invoke(null, Item.REGISTRY.getIDForObject(this.item)),
+            count,
+            itemDamage,
+            false)
+    NMS_ITEM_LOAD.invoke(item, serializeNBT())
+    return COPY_METHOD.invoke(null, item) as org.bukkit.inventory.ItemStack
+}
--- src/main/kotlin/skywolf46/refnbt/util/item/ItemNBTUtil.kt
+++ src/main/kotlin/skywolf46/refnbt/util/item/ItemNBTUtil.kt
@@ -6,18 +6,29 @@ import skywolf46.refnbt.impl.collections.CompoundNBTField
 import skywolf46.refnbt.util.BukkitVersionUtil
 
 object ItemNBTUtil {
+    private var errorNoticed = false
     private val ITEM_CLASS = BukkitVersionUtil.getOBCClass("inventory.CraftItemStack")
+
     private val HANDLE_FIELD = ITEM_CLASS.getDeclaredField("handle")
     private val SET_TAG_METHOD =
         BukkitVersionUtil.getNMSClass("ItemStack").getMethod("setTag", BukkitVersionUtil.getNMSClass("NBTTagCompound"))
     private val TAG_METHOD = BukkitVersionUtil.getNMSClass("ItemStack").getMethod("getTag")
+    private val ITEM_CONSTRUCTOR = BukkitVersionUtil.getNMSClass("ItemStack").getConstructor(
+        BukkitVersionUtil.getNMSClass("Item"),
+        Int::class.javaPrimitiveType,
+        Int::class.javaPrimitiveType,
+        Boolean::class.javaPrimitiveType
+    )
+
 
     init {
         HANDLE_FIELD.isAccessible = true
     }
 
     fun getOrCreateNBT(item: ItemStack): CompoundNBTField {
-        val handle = HANDLE_FIELD.get(item) ?: return CompoundNBTField()
+        val handle =
+            HANDLE_FIELD.get(item) ?: return CompoundNBTField()
+
         var tag = TAG_METHOD.invoke(handle)
         (tag == null){
             val comp = CompoundNBTField()
